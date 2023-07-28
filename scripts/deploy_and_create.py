7bdde5bf57714e5df24e1316bac1d7a0adc48cdf
sarobinson2011 <sarobinson2011@gmail.com>
2023-07-28 19:55:04 +0100

    deploy testing

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -4,7 +4,8 @@ from brownie import SimpleCollectible
 sample_token_uri = "https://ipfs.io/ipfs/QmddwNTy8cTSdCeaw81s1iWmRDzizB6aSBATEz7mt3edhx?filename=pug.json"
 labrador_uri = "https://ipfs.io/ipfs/QmNbLRXHLPwiP1A9syQxwhSMb1qocLjF3zNJVGL42t4DnS?filename=labrador.json"
 daschund_uri = "https://ipfs.io/ipfs/QmSz6eTfq9k9GWtvkKywFXAz6v3KT1wxKfEKDoqA3swFZT?filename=daschund.json"
-pug2_uri = "https://ipfs.io/ipfs/QmfHvKAoTY7xBUqD9z14kfS425fbVUXtUNxfm6c9N5Vrdd?filename=pug2.json"
+
+pug2_uri = "https://ipfs.io/ipfs/QmPRqc4NECw6HAybpxiPDBBpVDtNYvFS5vzdVErWHqTm3P?filename=pug2.json"
 
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
