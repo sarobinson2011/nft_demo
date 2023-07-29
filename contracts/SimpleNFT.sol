b09bf335198bb3e216eb4ea4f49d0e3f5c75c870
sarobinson2011 <sarobinson2011@gmail.com>
2023-07-29 17:03:58 +0100

    update README.md

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
 
-    constructor() public ERC721("DoggoPups", "ODG") {
+    constructor() public ERC721("DoggoPups", "DOGO") {
         tokenCounter = 0;
     }
 
--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -4,10 +4,10 @@ from brownie import SimpleCollectible
 sample_token_uri = "https://ipfs.io/ipfs/QmddwNTy8cTSdCeaw81s1iWmRDzizB6aSBATEz7mt3edhx?filename=pug.json"
 labrador_uri = "https://ipfs.io/ipfs/QmNbLRXHLPwiP1A9syQxwhSMb1qocLjF3zNJVGL42t4DnS?filename=labrador.json"
 daschund_uri = "https://ipfs.io/ipfs/QmSz6eTfq9k9GWtvkKywFXAz6v3KT1wxKfEKDoqA3swFZT?filename=daschund.json"
-
+jackrussell_uri = "https://ipfs.io/ipfs/QmVKdeYom7cF66zB8QHszeytgtzZRomrYmpuq11pR1M56v?filename=jackrussell.json"
 pug2_uri = "https://ipfs.io/ipfs/QmPRqc4NECw6HAybpxiPDBBpVDtNYvFS5vzdVErWHqTm3P?filename=pug2.json"
 
-OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+# OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
 def main():
@@ -16,9 +16,8 @@ def main():
         {"from": account}, publish_source=True
     )
 
-    for i in range(1):
-        tx = simple_collectible.createCollectible(daschund_uri, {"from": account})
-        tx.wait(1)
+    tx = simple_collectible.createCollectible(jackrussell_uri, {"from": account})
+    tx.wait(1)
 
 
 # continue video from --> 10:19:29  (test_simple_collectible.py)
